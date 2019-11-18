1bff5de496e5f066df195a880b2426e08e8a002c
Kazuki Yamagata <zsp2088dev>
2019-11-19 01:06:42 +0900

    最小単位のテキストを追加

---
+++ components/01_atoms/text/ExplanationText.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="explanation-text">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentsText',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ components/01_atoms/text/ItemText.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="item-text">
+    <span>{{ item }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemText',
+  props: {
+    item: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped></style>
