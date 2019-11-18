047cc4abde1e56b245f5f7ff9a8e5f86144c8ed8
Kazuki Yamagata <zsp2088dev>
2019-11-19 00:41:03 +0900

    重要項目の追加

---
+++ components/03_organisms/MyCareer.vue
@@ -0,0 +1,11 @@
+<template>
+  <p></p>
+</template>
+
+<script>
+export default {
+  name: 'MyCareer'
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>Init</p>
+  <div class="container">
+    <my-profile />
+    <my-skills />
+    <my-career />
+    <my-products />
+    <my-contact />
+  </div>
 </template>
+<script>
+import MyProfile from '../components/03_organisms/MyProfile'
+import MySkills from '../components/03_organisms/MySkills'
+import MyCareer from '../components/03_organisms/MyCareer'
+import MyProducts from '../components/03_organisms/MyProducts'
+import MyContact from '../components/03_organisms/MyContact'
+export default {
+  components: { MyContact, MyProducts, MyCareer, MySkills, MyProfile }
+}
+</script>
