94c7a9047f3d5281f472f977066222c79bd4a9fe
zsp2088dev <zsp2088dev@gmail.com>
2019-11-20 04:20:42 +0900

    スキル一覧をコンポーネント化

---
+++ components/02_molecules/SkillItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="skill-item">
+    <sub-title-text :title="title" />
+    <ul>
+      <li v-for="(skill, index) in skills" :key="index">
+        <item-text :item="skill" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SubTitleText from '../01_atoms/text/SubTitleText'
+import ItemText from '../01_atoms/text/ItemText'
+export default {
+  name: 'SkillItem',
+  components: { ItemText, SubTitleText },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    skills: {
+      type: Object,
+      default: () => {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
