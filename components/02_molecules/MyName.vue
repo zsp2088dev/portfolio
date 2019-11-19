b502efcea4bcc0fb74abd19b89f5f0cc935965ff
Kazuki Yamagata <zsp2088dev>
2019-11-19 12:11:28 +0900

    hrタグを削除

--- components/02_molecules/MyName.vue
+++ components/02_molecules/MyName.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="my-name">
     <sub-title-text></sub-title-text>
-    <sub-title-line></sub-title-line>
   </div>
 </template>
 
 <script>
 import SubTitleText from '../01_atoms/text/SubTitleText'
-import SubTitleLine from '../01_atoms/line/SubTitleLine'
 export default {
   name: 'MyName',
-  components: { SubTitleLine, SubTitleText }
+  components: { SubTitleText }
 }
 </script>
 
--- components/02_molecules/ProductCard.vue
+++ components/02_molecules/ProductCard.vue
@@ -2,19 +2,17 @@
   <div class="product-card">
     <sub-title-text></sub-title-text>
     <explanation-text></explanation-text>
-    <sub-title-line></sub-title-line>
     <date-text></date-text>
   </div>
 </template>
 
 <script>
 import SubTitleText from '../01_atoms/text/SubTitleText'
-import SubTitleLine from '../01_atoms/line/SubTitleLine'
 import DateText from '../01_atoms/text/DateText'
 import ExplanationText from '../01_atoms/text/ExplanationText'
 export default {
   name: 'ProductCard',
-  components: { ExplanationText, DateText, SubTitleLine, SubTitleText }
+  components: { ExplanationText, DateText, SubTitleText }
 }
 </script>
 
