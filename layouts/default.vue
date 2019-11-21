492b80ad1ae514fd449c56f7e844ff2b9120328e
Kazuki Yamagata <zsp2088dev>
2019-11-21 16:51:13 +0900

    全体的なレイアウトの更新

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,21 @@
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 html {
   font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue',
-    'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
+    'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', Roboto, Arial,
+    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  text-align: center;
 }
 </style>
